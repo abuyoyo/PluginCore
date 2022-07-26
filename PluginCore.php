7929889414fcba0ffbc944064a827fc284cca87c
abuoyoyo <abuyoyo@users.noreply.github.com>
2022-07-27 01:20:07 +0300

    misc - notes + lint

--- PluginCore.php
+++ PluginCore.php
@@ -11,7 +11,7 @@ use function register_uninstall_hook;
 
 defined( 'ABSPATH' ) || die( 'No soup for you!' );
 
-if( ! function_exists('get_plugin_data') ){
+if( ! function_exists('get_plugin_data') ) {
 	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 }
 
@@ -27,9 +27,8 @@ if ( ! class_exists( 'WPHelper/PluginCore' ) ):
  * @version 0.17
  * 
  * @todo plugin_action_links - on Plugins page
- * 
  */
-class PluginCore{
+class PluginCore {
 
 	/**
 	 * @var string 
@@ -106,37 +105,43 @@ class PluginCore{
 	 */
 	static $cores = [];
 
-	static public function get($slug){
+	/**
+	 * Retrieve instance of PluginCore by plugin slug.
+	 * 
+	 * @param string $slug - Plugin slug
+	 * @return PluginCore - Instance of specific plugin.
+	 */
+	static public function get( $slug ) {
 		return self::$cores[$slug] ? self::$cores[$slug] : null;
 	}
 
-	function __construct( $plugin_file, $options = null ){
+	function __construct( $plugin_file, $options = null ) {
 
 		$this->plugin_file( $plugin_file );
 
-		if ( is_array($options) && ! empty($options) ){
+		if ( is_array( $options ) && ! empty( $options ) ) {
 
 			$options = (object) $options;
 
-			if ( isset( $options->title ) ){
+			if ( isset( $options->title ) ) {
 				$this->title( $options->title );
 			}else{
 				$this->title(); // get title from header plugin_data
 			}
 
-			if ( isset( $options->slug ) ){
+			if ( isset( $options->slug ) ) {
 				$this->slug( $options->slug );
 			}else{
 				$this->slug(); // guess slug from plugin basename
 			}
 			
-			if ( isset( $options->const ) ){
+			if ( isset( $options->const ) ) {
 				$this->const( $options->const );
 			}else{
 				$this->const();
 			}
 
-			if ( isset( $options->admin_page ) ){
+			if ( isset( $options->admin_page ) ) {
 				$this->admin_page( $options->admin_page );
 			}
 
@@ -152,7 +157,7 @@ class PluginCore{
 			if ( isset( $options->upgrade_cb ) )
 				$this->upgrade_cb( $options->upgrade_cb );
 
-			if ( isset( $options->update_checker ) ){
+			if ( isset( $options->update_checker ) ) {
 				$this->update_checker( $options->update_checker );
 			}
 		}
@@ -162,11 +167,18 @@ class PluginCore{
 	}
 
 	/**
-	 * @todo set plugin_dir_path, plugin_basename as accessible public variables
+	 * Define plugin constants (_PATH, _URL, _BASENAME, _FILE etc.)
+	 * Register activation, deactivation, uninstall, upgrade hooks.
+	 * Init PUC update checker.
+	 * Add PluginCore instance to static $cores
+	 * 
+	 * @todo rename method bootstrap()
+	 * @todo set plugin_dir_path, plugin_basename as accessible public variables (available thru methods atm)
 	 */
-	function setup(){
+	function setup() {
 
 		// init path and url
+		// redundant init() and path() are getter/setter methods.
 		$this->path();
 		$this->url();
 
@@ -181,10 +193,10 @@ class PluginCore{
 
 		$this->register_hooks();
 
-		self::$cores[$this->slug()] = $this; // using slug() method
+		self::$cores[ $this->slug() ] = $this; // using slug() method
 
 
-		if ( $this->update_checker == true ){
+		if ( $this->update_checker == true ) {
 			// run early - before Puc_v4p8_Scheduler->maybeCheckForUpdates() [admin_init 10]
 			// hooking on admin_init does not work with wp_plugin_updates as it requires user logged-in
 			// add_action( 'admin_init', [$this, 'init_update_checker'], 9 );
@@ -192,7 +204,7 @@ class PluginCore{
 		}
 	}
 
-	private function register_hooks(){
+	private function register_hooks() {
 
 		if ( ! empty( $this->activate_cb ) ) // && is_callable() ?
 			register_activation_hook( $this->plugin_file, $this->activate_cb );
@@ -204,17 +216,25 @@ class PluginCore{
 			register_uninstall_hook( $this->plugin_file, $this->uninstall_cb );
 
 		if ( ! empty( $this->upgrade_cb ) )
-			add_action( 'upgrader_process_complete', [$this, 'upgrade_cb_wrapper'], 10, 2 );
+			add_action( 'upgrader_process_complete', [ $this, 'upgrade_cb_wrapper' ], 10, 2 );
 	}
 
-	public function title( $title=null ){
+	/**
+	 * Getter/Setter - title
+	 * Plugin title.
+	 * If none provided - plugin header Title will be used.
+	 * 
+	 * @param  string|null $title
+	 * @return string      $this->title
+	 */
+	public function title( $title = null ) {
 		
-		if ( ! empty( $title ) ){
+		if ( ! empty( $title ) ) {
 			$title = esc_html( $title );
 			$this->title = $title;
 		}	
 
-		if ( empty( $this->title ) ){
+		if ( empty( $this->title ) ) {
 			$this->plugin_data();
 			$this->title = $this->plugin_data['Title'];
 		}
@@ -225,11 +245,19 @@ class PluginCore{
 	/**
 	 * Wrapper function for $this->title()
 	 */
-	public function name( $title=null ){
+	public function name( $title = null ) {
 		return $this->title( $title );
 	}
 
-	public function slug( $slug=null ){
+	/**
+	 * Getter/Setter - slug
+	 * Plugin slug.
+	 * If none provided - plugin file basename will be used
+	 * 
+	 * @param  string|null $slug
+	 * @return string      $this->slug
+	 */
+	public function slug( $slug = null ) {
 		if ( ! empty( $slug ) )
 			$this->slug = $slug;
 
@@ -241,7 +269,14 @@ class PluginCore{
 		return $this->slug;
 	}
 
-	public function plugin_file( $plugin_file ){
+	/**
+	 * Setter - plugin_file (also Getter - kinda)
+	 * Plugin file fully qualified path.
+	 * 
+	 * @param  string $plugin_file - Path to plugin file
+	 * @return string $this->plugin_file
+	 */
+	public function plugin_file( $plugin_file ) {
 		$this->plugin_file = $plugin_file;
 		return $this->plugin_file;
 	}
@@ -253,44 +288,52 @@ class PluginCore{
 	 * 
 	 * @todo revisit this
 	 */
-	public function file(){
+	public function file() {
 		return $this->plugin_file;
 	}
 
 
-	public function plugin_data(){
-		if ( empty( $this->plugin_data ) ){
+	public function plugin_data() {
+		if ( empty( $this->plugin_data ) ) {
 			$this->plugin_data = get_plugin_data( $this->plugin_file, false);
 		}
 		return $this->plugin_data;
 	}
 
-	public function const( $const=null ){
+	/**
+	 * Getter/Setter - const
+	 * Prefix of plugin specific defines (PLUGIN_NAME_PATH etc.)
+	 * If not provided - plugin slug will be uppercased.
+	 * 
+	 * @param  string|null $const (string should be uppercased)
+	 * @return string      $this->const
+	 */
+	public function const( $const = null ) {
 		
 		// if $const provided - use that
-		if ( ! empty( $const ) ){
+		if ( ! empty( $const ) ) {
 			$this->const = $const;
 		}
 
 		// if no $const provided - generate from slug()
-		if ( empty( $this->const ) ){
-			$this->const = str_replace( '-', '_' , strtoupper( $this->slug() ) ); // using slug() method
+		if ( empty( $this->const ) ) {
+			$this->const = str_replace( '-', '_' , strtoupper( $this->slug() ) ); // using slug() getter/setter
 		}
 		
 		return $this->const;
 	}
 
-	public function admin_page( $admin_page ){
-		if ( empty( $admin_page['slug'] ) ){
+	public function admin_page( $admin_page ) {
+		if ( empty( $admin_page['slug'] ) ) {
 			$admin_page['slug'] = $this->slug();
 		}
-		if ( empty( $admin_page['title'] ) ){
+		if ( empty( $admin_page['title'] ) ) {
 			$admin_page['title'] = $this->title();
 		}
 
-		$this->admin_page = new AdminPage($admin_page);
+		$this->admin_page = new AdminPage( $admin_page );
 
-		if ( method_exists( $this->admin_page, 'plugin_core' ) ){
+		if ( method_exists( $this->admin_page, 'plugin_core' ) ) {
 			$this->admin_page->plugin_core( $this );
 		}
 
@@ -298,81 +341,81 @@ class PluginCore{
 	}
 
 
-	public function path(){
+	public function path() {
 		if ( empty( $this->path ) )
 			$this->path = plugin_dir_path( $this->plugin_file );
 		return $this->path;
 	}
 
-	public function url(){
+	public function url() {
 		if ( empty( $this->url ) )
 			$this->url = plugin_dir_url( $this->plugin_file );
 		return $this->url;
 	}
 
-	public function plugin_basename(){
+	public function plugin_basename() {
 		if ( empty( $this->plugin_basename ) )
 			$this->plugin_basename = plugin_basename( $this->plugin_file );
 		return $this->plugin_basename;
 	}
 
-	private function activate_cb( $activate_cb ){
+	private function activate_cb( $activate_cb ) {
 		// test is_callable() ? or is it too soon?
 		$this->activate_cb = $activate_cb;
 	}
 
-	private function deactivate_cb( $deactivate_cb ){
+	private function deactivate_cb( $deactivate_cb ) {
 		// test is_callable() ? or is it too soon?
 		$this->deactivate_cb = $deactivate_cb;
 	}
 
-	private function uninstall_cb( $uninstall_cb ){
+	private function uninstall_cb( $uninstall_cb ) {
 		// test is_callable() ? or is it too soon?
 		$this->uninstall_cb = $uninstall_cb;
 	}
 
-	private function upgrade_cb( $upgrade_cb ){
+	private function upgrade_cb( $upgrade_cb ) {
 		// test is_callable() ? or is it too soon?
 		$this->upgrade_cb = $upgrade_cb;
 	}
 
-	private function update_checker( $update_checker ){
+	private function update_checker( $update_checker ) {
 		// Puc_v4_Factory::buildUpdateChecker
-		if ( empty( $update_checker ) ){
+		if ( empty( $update_checker ) ) {
 			$this->update_checker = false;
 		}else{
-			if ( is_bool( $update_checker ) ){
+			if ( is_bool( $update_checker ) ) {
 				$this->update_checker = $update_checker;
 			}
 
-			if ( is_string( $update_checker ) ){
+			if ( is_string( $update_checker ) ) {
 				$this->update_checker = true;
 				$this->update_repo_uri = $update_checker;
 			}
 
-			if ( is_array( $update_checker ) ){
+			if ( is_array( $update_checker ) ) {
 				$this->update_checker = true;
 
-				if ( isset( $update_checker['uri'] ) ){
+				if ( isset( $update_checker['uri'] ) ) {
 					$this->update_repo_uri = $update_checker['uri'];
 				}
-				if ( isset( $update_checker['auth'] ) ){
+				if ( isset( $update_checker['auth'] ) ) {
 					$this->update_auth = $update_checker['auth'];
 				}
 
-				if ( isset( $update_checker['branch'] ) ){
+				if ( isset( $update_checker['branch'] ) ) {
 					$this->update_branch = $update_checker['branch'];
 				}
 			}
 		}
 	}
 
-	public function init_update_checker(){
+	public function init_update_checker() {
 	
 		if ( ! class_exists('Puc_v4_Factory') )
 			return;
 
-		if ( ! isset( $this->update_repo_uri ) ){
+		if ( ! isset( $this->update_repo_uri ) ) {
 			$this->plugin_data();
 			
 			if ( isset( $this->plugin_data['PluginURI'] ) )
@@ -400,12 +443,12 @@ class PluginCore{
 	/**
 	 * upgrade_cb_wrapper
 	 * 
-	 * This function only called if upgrade_cb is set (@see regiseter_hooks())
+	 * This function only called if upgrade_cb is set (@see register_hooks())
 	 * This function called on upgrader_process_complete
 	 * sanity-checks if our plugin was upgraded
 	 * if so - calls upgrade_cb provided by our plugin
 	 */
-	public function upgrade_cb_wrapper( $upgrader_object, $options ){
+	public function upgrade_cb_wrapper( $upgrader_object, $options ) {
 		if(
 			$options['action'] == 'update'  // has upgrade taken place
 			&&
